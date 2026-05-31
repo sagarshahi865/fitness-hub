003330faec7565b8fc0fee568867e3f010e28193
Jeevan Neupane <jeevannar16@gmail.com>
2026-05-31 15:04:56 +0545

    Commited after solving not loading runserver

--- fitness_hub/settings.py
+++ fitness_hub/settings.py
@@ -23,12 +23,9 @@ INSTALLED_APPS = [
     'inspiration',
     
     # Third Party
-    'crispy_forms',
-    'crispy_bootstrap5',
+    
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
