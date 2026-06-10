03fcfea9c2b2d2f1a0f59e65d6defaad39eb40d5
sagarshahi865 <sagarshahi865@gmail.com>
2026-06-10 09:17:48 +0545

    Add social connections feature: players directory, friend/rival/partner requests

--- gamification/urls.py
+++ gamification/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('quests/<int:uq_id>/claim/', views.claim_quest_view, name='claim-quest'),
     path('quests/refresh/', views.refresh_quests, name='refresh-quests'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('players/', views.players, name='players'),
+    path('connections/', views.connections, name='connections'),
+    path('connections/send/', views.send_request, name='send-request'),
+    path('connections/respond/', views.respond_request, name='respond-request'),
     path('api/summary/', views.api_summary, name='api-summary'),
     path('api/award-debug/', views.api_award_debug, name='api-award-debug'),
 ]
